4dd7b1bb59d40b001825b77d0fb58854ed1a9024
jameel hanomdeh <jameel.jalal8@gmail.com>
2025-09-24 15:50:05 +0300

    add title to the website

--- shababaltageerr/frontend/src/componnet/Footer.jsx
+++ shababaltageerr/frontend/src/componnet/Footer.jsx
@@ -2,17 +2,18 @@ import style from "../style/Footer.module.css";
 import CopyrightIcon from '@mui/icons-material/Copyright';
 export default function Footer(){
     let whatsapp="https://wa.me/962791084778";
-    let inastagram="https://www.instagram.com/shabab_altageer/"
+    let inastagram="https://www.instagram.com/shabab_altageer/";
+    let telegram="t.me/+hg4huqIpWxdhOWM0";
     return(
         <div className={style.footer}>
             <div className={style.footerinformation}>
-                  <strong>شباب التغير</strong>
+                  <strong>شباب التغيير</strong>
             <img width={50} src="./380734428_324908939935661_8355368135205759861_n.jpg"></img>
             </div>
             <div className={style.icon}>
                 <a href={inastagram}><img   src="./Instagram-Logo.png"></img></a>
                  <a href={whatsapp}><img src="./whatsapp-logo-icon-isolated-on-transparent-background-free-png.webp"></img></a>
-                 {/* <a><img  src="./facebook-logo-facebook-icon-transparent-free-png.webp"></img></a> */}
+                 <a href={telegram}><img   src="./telegram_PNG12.png"></img></a>
             </div>
           {/* <p>جميع الحقوق محفوظة <CopyrightIcon/></p> */}
         </div>
