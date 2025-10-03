f8f26c074f16e676308522a728f807383378d862
jameel hanomdeh <jameel.jalal8@gmail.com>
2025-10-04 02:47:59 +0300

    edit url

--- shababaltageerr/backend/index.js
+++ shababaltageerr/backend/index.js
@@ -11,7 +11,7 @@ const app=express();
 
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173"
+    origin:"https://shababaltageer.vercel.app"
 }));
 
 app.use((req,res,next)=>{
