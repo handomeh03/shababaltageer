0a3b2bae54c0cdbed39cae66703e0b847c7351f3
jameel hanomdeh <jameel.jalal8@gmail.com>
2025-10-05 18:48:11 +0300

    edit dist

--- shababaltageerr/backend/index.js
+++ shababaltageerr/backend/index.js
@@ -40,7 +40,7 @@ app.use("/api/event",eventRouter);
 app.use("/api/volunter",VolunterRouter);
 app.use("/api/receipt",receiptRouter);
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "frontend/dist", "index.html"));
 });
 initdb().then(
     app.listen(process.env.PORT || 3000,()=>{
