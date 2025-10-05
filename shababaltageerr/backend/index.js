dc4125e702f7a49eefd94ce83316ef63aa3aeb5a
jameel hanomdeh <jameel.jalal8@gmail.com>
2025-10-05 18:45:37 +0300

    add dist

--- shababaltageerr/backend/index.js
+++ shababaltageerr/backend/index.js
@@ -28,7 +28,7 @@ app.use((req,res,next)=>{
 
 app.use("/uploads", express.static("uploads"));
 
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
 app.get("/", (req, res) => {
   res.send("Hello World");
